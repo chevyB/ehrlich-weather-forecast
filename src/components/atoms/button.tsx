0f511b960fced6aade17fe5adfd9c6cf696a1a93
chevyB <chevbanico@gmail.com>
2022-10-29 12:29:11 +0800

    [Feature] HomePage Markup

--- src/components/atoms/button.tsx
+++ src/components/atoms/button.tsx
@@ -2,13 +2,14 @@ import type { ReactNode } from 'react';
 
 interface AppProps {
   children: ReactNode;
+  type?: 'button' | 'reset' | 'submit';
 }
 
-const Button = ({ children }: AppProps) => {
+const Button = ({ children, type = 'button' }: AppProps) => {
   return (
     <button
-      type="button"
-      className="text-white bg-blue-700 hover:bg-blue-800 rounded-md text-md px-5 py-2 mr-2 dark:bg-blue-600 "
+      type={type}
+      className="text-white bg-blue-700 hover:bg-blue-800 rounded-md text-md px-5 py-2 mr-2 dark:bg-blue-600"
     >
       {children}
     </button>
