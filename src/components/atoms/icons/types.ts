4ea8f62be8893b7d1d6017d1375bd10334006b3e
chevyB <chevbanico@gmail.com>
2022-10-29 11:48:59 +0800

    Revert "[Feature] HomePage Markup"

--- src/components/atoms/icons/types.ts
+++
@@ -1,9 +0,0 @@
-import type { SVGAttributes } from 'react';
-
-export interface Iprops extends SVGAttributes<SVGElement> {
-  color?: string;
-  size?: string | number;
-  fill?: string;
-  width?: string | number;
-  height?: string | number;
-}
